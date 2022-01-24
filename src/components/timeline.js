9eb14a7f6de5b6325f5bfc15bc9117cf42e9dd92
manoj ravichandran <manojravichandran12@gmail.com>
2022-01-24 15:56:09 +0530

    done

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -23,31 +23,31 @@ export class timeline extends React.PureComponent {
                                  <tr>
                                      <td className="colu-1">1</td>
                                      <td className="colu-2">Phase 1 Submission(Problem statement)</td>
-                                     <td className="colu-3">20-JAN-2021</td>
-                                     <td className="colu-4">22-FEB-2021</td>
+                                     <td className="colu-3">24-JAN-2021</td>
+                                     <td className="colu-4">18-FEB-2021</td>
                                  </tr>
                                  </tbody>
                                  <tbody>
                                  <tr>
                                      <td className="colu-1">2</td>
                                      <td className="colu-2">Phase 1 Screening</td>
-                                     <td className="colu-3">21-FEB-2021</td>
-                                     <td className="colu-4">26-FEB-2021</td>
+                                     <td className="colu-3">19-FEB-2021</td>
+                                     <td className="colu-4">23-FEB-2021</td>
                                  </tr>
                                  </tbody>
                                  <tbody>
                                  <tr>
                                      <td className="colu-1">3</td>
                                      <td className="colu-2">Declaration of Shortlisted Candidates for Phase 2</td>
-                                     <td className="colu-3">27-FEB-2021</td>
-                                     <td className="colu-4">28-FEB-2021</td>
+                                     <td className="colu-3">24-FEB-2021</td>
+                                     <td className="colu-4">25-FEB-2021</td>
                                  </tr>
                                  </tbody>
                                  <tbody>
                                  <tr>
                                      <td className="colu-1">4</td>
                                      <td className="colu-2">Phase 2 Registration</td>
-                                     <td className="colu-3">01-MAR-2021</td>
+                                     <td className="colu-3">26-FEB-2021</td>
                                      <td className="colu-4">05-MAR-2021</td>
                                  </tr>
                                  </tbody>
@@ -55,8 +55,8 @@ export class timeline extends React.PureComponent {
                                  <tr>
                                      <td className="colu-1">5</td>
                                      <td className="colu-2">Hack and Tackle 3.0</td>
-                                     <td className="colu-3">15-MAR-2021</td>
-                                     <td className="colu-4">16-MAR-2021</td>
+                                     <td className="colu-3">10-MAR-2021</td>
+                                     <td className="colu-4">11-MAR-2021</td>
                                  </tr>
                                  </tfoot>
                              </table>
